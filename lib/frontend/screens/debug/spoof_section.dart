d10362b460c84f23102357685df16faa2975d5dc
Davidgo134 <davidpopov2121@gmail.com>
2026-09-11 00:20:59 +0300

    fix: add missing spoof_section.dart stub for debug menu

---
+++ lib/frontend/screens/debug/spoof_section.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Заглушка секции «Spoof» в debug-меню.
+/// TODO: реализовать настройки подмены (статус/профиль).
+class DebugSpoofSection extends StatelessWidget {
+  const DebugSpoofSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
